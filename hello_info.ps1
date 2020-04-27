a1e875989b37d0ea4f7708b8b88f8ef2691fa9ae
Tim <tim.e.mcguire@gmail.com>
2020-04-27 15:42:56 -0500

    basic_usage

---
+++ hello_info.ps1
@@ -0,0 +1,23 @@
+# imports
+
+# Hello and info
+
+# write-host and write-output
+
+# print powershell version
+
+# script execution settings
+
+
+# Variables
+
+# Loops
+
+# Conditionals
+
+# Objects
+
+# files
+
+# code from older versions
+
