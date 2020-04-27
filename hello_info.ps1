89dc312e1a2d03b2d0528c946c25467c4e77838a
Tim <tim.e.mcguire@gmail.com>
2020-04-27 17:11:53 -0500

    simple statements

--- hello_info.ps1
+++ hello_info.ps1
@@ -1,19 +1,35 @@
 # imports
 
+#powershell commands follow a pattern of VERB-NOUN
 # Hello and info
 
 # write-host and write-output
 
 # print powershell version
+# because get-host.version should be avoided
+write-host "Powershell Version: "
+write-host $PSVersionTable.PSVersion
 
-# script execution settings
 
+# script execution settings (Execution Policy)
+# The following gets the "effective" execution policy
+write-host get-executionPolicy
 
 # Variables
-
-# Loops
+#declare:
+$x = 23
+#output
+write-host $x
 
 # Conditionals
+# instead of < > = != etc., need to use -lt and -eq
+
+# Loops
+#simple for loop
+for ($x = 0; $x < 100; $x++){
+  write-host $x
+}
+
 
 # Objects
 
