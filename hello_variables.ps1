37454fad237fb91a62d48f3cadcbf62fb15d9245
Tim <tim.e.mcguire@gmail.com>
2020-04-28 09:23:53 -0500

    Create hello_variables.ps1

---
+++ hello_variables.ps1
@@ -0,0 +1,22 @@
+# A few examples demonstrating variables
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_variables?view=powershell-7
+
+# Declare a variable
+$x = 10
+$lastName = "McGyver"
+$arr = 1,2,3,4,5
+
+# render variables
+$x
+# Output:
+10
+
+$lastName
+# Output:
+McGyver
+
+write-host $lastName
+#Output:
+McGyver
+
+
