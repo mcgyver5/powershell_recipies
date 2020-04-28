4bcda025b496e6f963793ac0d64a2a1d23da7cbc
Tim <tim.e.mcguire@gmail.com>
2020-04-28 13:57:46 -0500

    Update hello_info.ps1

--- hello_info.ps1
+++ hello_info.ps1
@@ -2,38 +2,17 @@
 
 #powershell commands follow a pattern of VERB-NOUN
 # Hello and info
-
-# write-host and write-output
+"HELLO WORLD"
 
 # print powershell version
 # because get-host.version should be avoided
-write-host "Powershell Version: "
-write-host $PSVersionTable.PSVersion
+"Powershell Version: "
+$PSVersionTable.PSVersion
 
 
 # script execution settings (Execution Policy)
 # The following gets the "effective" execution policy
-write-host get-executionPolicy
+get-executionPolicy
 
-# Variables
-#declare:
-$x = 23
-#output
-write-host $x
-
-# Conditionals
-# instead of < > = != etc., need to use -lt and -eq
-
-# Loops
-#simple for loop
-for ($x = 0; $x < 100; $x++){
-  write-host $x
-}
-
-
-# Objects
-
-# files
-
-# code from older versions
-
+# The following shows all currently defined variables
+get-variable
